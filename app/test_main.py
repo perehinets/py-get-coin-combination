b5dce6450c0141984b55250c28ac8747771d549c
Vasyl Perehinets <perehinets95@gmail.com>
2026-07-29 18:08:51 +0100

    new solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,10 +5,14 @@ from app.main import get_coin_combination
 @pytest.mark.parametrize(
     "count_of_coins,expected_combination",
     [
+        (0, [0, 0, 0, 0]),
         (1, [1, 0, 0, 0]),
         (6, [1, 1, 0, 0]),
         (17, [2, 1, 1, 0]),
         (50, [0, 0, 0, 2]),
+        (99, [4, 0, 2, 3]),
+        (120, [0, 0, 2, 4]),
+        (41, [1, 1, 1, 1])
     ]
 )
 def test_get_coin_combination(count_of_coins: int,
